93d61500c92d5f4c412eaf3105768414705c3057
Yusif Salam-zade <yusif.salam-zade@aalto.fi>
2019-05-16 16:45:21 +0300

    part 3

---
+++ part3/3_06/maiden_tiedot/src/components/Country.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import CountryFull from './CountryFull'
+
+const Country = ({ country }) => {
+
+
+    return (
+        <div>
+            {country.name}
+        </div>
+    );
+};
+
+export default Country;
